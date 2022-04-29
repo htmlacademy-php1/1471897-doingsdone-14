7afd0806f2b3c64a5d8ddd911cedf0304d4bb40f
PavelPolin <pavel.polin.59@gmail.com>
2022-04-29 13:30:31 +0500

    Создание схемы БД личного проекта

--- schema.sql
+++ schema.sql
@@ -4,17 +4,24 @@ CREATE DATABASE dela_v_poryadke
 
 CREATE TABLE projects (
     id INT AUTO_INCREMENT,
-    name VARCHAR(32) NOT NULL
+    user_id INT,
+    name VARCHAR(32) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
  );
 
 CREATE TABLE tasks (
+    id INT AUTO_INCREMENT,
+    project_id INT,
+    user_id INT,
     creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status BOOL DEFAULT FALSE,
     name VARCHAR(32) NOT NULL,
     file VARCHAR(2048),
     deadline TIMESTAMP,
-    project_id INT,
-    user_id INT
+    PRIMARY KEY (id),
+    FOREIGN KEY (project_id) REFERENCES projects (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
  );
 
 CREATE TABLE users (
@@ -22,7 +29,8 @@ CREATE TABLE users (
     registration TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     email VARCHAR(128) NOT NULL UNIQUE,
     name VARCHAR(128) NOT NULL,
-    password CHAR(32) NOT NULL
+    password CHAR(32) NOT NULL,
+    PRIMARY KEY (id)
  );
 
 CREATE INDEX task_name ON tasks(name);
